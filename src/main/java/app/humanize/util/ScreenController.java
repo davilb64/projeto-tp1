d77a73cb26fb6e91e0a46cc86190c5e3b8ee3d3e
Davi Lopes Brito <davilopesbrito64@gmail.com>
2025-10-23 22:46:16 -0300

    Adiciona estilização (Icones)

--- src/main/java/app/humanize/util/ScreenController.java
+++ src/main/java/app/humanize/util/ScreenController.java
@@ -3,6 +3,7 @@ package app.humanize.util;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,8 +17,11 @@ public class ScreenController {
     }
 
     public static void changeScene(String fxmlPath) {
+        Image image = new Image("/humanize-logo.png");
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(ScreenController.class.getResource(fxmlPath)));
+            stage.setTitle("Humanize");
+            stage.getIcons().add(image);
             stage.setScene(new Scene(root, 800, 650));
             stage.show();
         } catch (IOException e) {
